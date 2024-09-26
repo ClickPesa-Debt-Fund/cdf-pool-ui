2d39745f70b9f38eec07b3fa6c43a5845d6e3ea5
Benedict Steven <bmsteve96@gmail.com>
2024-09-26 10:11:41 +0300

    IMPROVE: add doppler config for env variables

--- src/pages/backstop/index.tsx
+++ src/pages/backstop/index.tsx
@@ -1,7 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
 const Backstop = () => {
+  const navigate = useNavigate();
   return (
     <div className="md:space-y-[30px] space-y-[24px] md:py-[100px] py-[90px] container max-w-[1270px] min-h-full">
-      Backstop Page
+      <div>
+        <Button
+          variant={"link"}
+          className="gap-2 -ml-4"
+          onClick={() => navigate("/dashboard")}
+        >
+          <ArrowLeft size={20} /> Dashboard
+        </Button>
+      </div>
+      <div>Backstop Page</div>
     </div>
   );
 };
